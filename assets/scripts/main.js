232a1444b0ca71662d34ed4a1de97ea9b8019f07
Estella Li <liqx9979@gmail.com>
2022-11-21 16:30:55 -0800

    update part2

--- assets/scripts/main.js
+++ assets/scripts/main.js
@@ -1,7 +1,5 @@
 // main.js
 
-const { resolve } = require("path");
-
 // CONSTANTS
 const RECIPE_URLS = [
   'https://introweb.tech/assets/json/1_50-thanksgiving-side-dishes.json',
@@ -47,15 +45,28 @@ function initializeServiceWorker() {
   // We first must register our ServiceWorker here before any of the code in
   // sw.js is executed.
   // B1. TODO - Check if 'serviceWorker' is supported in the current browser
+  if( "serviceWorker" in navigator){
   // B2. TODO - Listen for the 'load' event on the window object.
+  addEventListener('load', async() =>{
   // Steps B3-B6 will be *inside* the event listener's function created in B2
   // B3. TODO - Register '/sw.js' as a service worker (The MDN article
   //            "Using Service Workers" will help you here)
+  try{
+    let registration = await navigator.serviceWorker.register('/sw.js')
   // B4. TODO - Once the service worker has been successfully registered, console
   //            log that it was successful.
+    if(registration.active){
+      console.log("service worker has been successfully registered");
+    }
+  }
   // B5. TODO - In the event that the service worker registration fails, console
   //            log that it has failed.
+      catch(error){
+        console.error(`Registration failed with ${error}`);
+      }
   // STEPS B6 ONWARDS WILL BE IN /sw.js
+    })
+  }
 }
 
 /**
@@ -83,15 +94,14 @@ async function getRecipes() {
   //            function (we call these callback functions). That function will
   //            take two parameters - resolve, and reject. These are functions
   //            you can call to either resolve the Promise or Reject it.
-  return new Promise((resolve,reject) => {
+  return new Promise(async(resolve,reject) => {
   /**************************/
   // A4-A11 will all be *inside* the callback function we passed to the Promise
   // we're returning
   /**************************/
   // A4. TODO - Loop through each recipe in the RECIPE_URLS array constant
   //            declared above
-    for( let i = 0; i < RECIPE_URLS.length(); i++ ){
-      const recipe_url = RECIPE_URLS[i];
+    for( let recipe_url of RECIPE_URLS){
       // A5. TODO - Since we are going to be dealing with asynchronous code, create
       //            a try / catch block. A6-A9 will be in the try portion, A10-A11
       //            will be in the catch portion.
@@ -100,23 +110,28 @@ async function getRecipes() {
       //            article on fetch(). NOTE: Fetches are ASYNCHRONOUS, meaning that
       //            you must either use "await fetch(...)" or "fetch.then(...)". This
       //            function is using the async keyword so we recommend "await"
-      const response = await fetch('https://introweb.tech/assets/json/1_50-thanksgiving-side-dishes.json');
+        let response = await fetch(recipe_url);
       // A7. TODO - For each fetch response, retrieve the JSON from it using .json().
       //            NOTE: .json() is ALSO asynchronous, so you will need to use
       //            "await" again
-      if( response.ok ){
-        const jsonValue = await response.json();
-      }
+        let jsonValue = await response.json();
       // A8. TODO - Add the new recipe to the recipes array
-      newRecipes.push(newRECIPE);
+        newRecipes.push(jsonValue);
       // A9. TODO - Check to see if you have finished retrieving all of the recipes,
       //            if you have, then save the recipes to storage using the function
       //            we have provided. Then, pass the recipes array to the Promise's
       //            resolve() method.
-      } catch(){
+      if( recipe_url == RECIPE_URLS[RECIPE_URLS.length-1]) {
+        saveRecipesToStorage(newRecipes);
+        resolve(newRecipes);
+      }
+    } 
 
   // A10. TODO - Log any errors from catch using console.error
+      catch(error){
+        console.log(error);
   // A11. TODO - Pass any errors to the Promise's reject() function
+        reject(error);
       }
     }
   });
